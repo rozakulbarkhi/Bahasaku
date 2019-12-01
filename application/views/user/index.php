8a2bd080f871834ef21a6649ab153352b7c77362
Rozakul Barkhi <49378591+rozakulbarkhi@users.noreply.github.com>
2019-12-02 00:04:16 +0700

    Update index.php

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -37,54 +37,94 @@
 	<br><br><br>
 
 	<div class="container">
-		<h2 class="text-center"><strong>Kenapa harus memilih kami?</strong></h2>
-		<hr class="hr-fitur">
-		<div class="row">
-			<div class="col-md-6">
-				<h4 class="pt-5 pr-5"><strong>Berfokus pada Bahasa Indonesia</strong></h4>
-				<p class="pr-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ducimus quam illum
-					similique cumque ratione cupiditate dolorem voluptate pariatur hic, rerum, et tempora earum iure
-					ullam, id ea numquam, soluta!</p>
-			</div>
-			<div class="col-md-6">
-				<img src="<?= base_url('assets/img/target.png'); ?>" width="200" alt="">
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<img src="<?= base_url('assets/img/gratiss.png'); ?>" width=" 200" alt="">
-			</div>
-			<div class="col-md-6">
-				<h4 class="pt-5 pr-5"><strong>Bebas Biaya</strong></h4>
-				<p class="pr-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ducimus quam illum
-					similique cumque ratione cupiditate dolorem voluptate pariatur hic, rerum, et tempora earum iure
-					ullam, id ea numquam, soluta!</p>
-			</div>
-		</div>
+		<!-- testimoni -->
+		<h2 class="text-center"><strong>Kata Mereka?</strong></h2>
+		<hr class="hr-about">
 		<br>
 		<div class="row">
-			<div class="col-md-6">
-				<h4 class="pt-5"><strong>Untuk Segala Jenjang</strong></h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ducimus quam illum
-					similique cumque ratione cupiditate dolorem voluptate pariatur hic, rerum, et tempora earum iure
-					ullam, id ea numquam, soluta!</p>
+			<div class="col-md-4">
+				<div class="img-thumbnail">
+					<img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/1200px-SpongeBob_SquarePants_character.svg.png" alt="" class="img-fluid rounded-circle ml-5 mt-3" width="250">
+					<br><br>
+					<div class="caption">
+						<p class="text-center">"Dengan materi yang berkualitas dari <i>Bahasaku</i>,
+							saya bisa lebih mudah memahami mata pelajaran Bahasa Indonesia" - <strong>Spongebob</strong>, <i>Ilmu Kelautan</i></p>
+					</div>
+				</div>
 			</div>
-			<div class="col-md-6">
-				<img src="<?= base_url('assets/img/luas.png'); ?>" width="200" alt="">
+
+			<div class="col-md-4">
+				<div class="img-thumbnail">
+					<img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/33/Patrick_Star.svg/1200px-Patrick_Star.svg.png" alt="" class="img-fluid rounded-circle ml-5 mt-3" width="250">
+					<br><br>
+					<div class="caption">
+						<p class="text-center">"<i>Bahasaku</i> menerapkan metode pembelajaran yang berfokus pada konsep,
+							jadi tidak hanya hafalan saja. Saya suka! - <strong>Patrick Star</strong>, <i>Teknik Menyelam</i>"</p>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<img src="<?= base_url('assets/img/random.png'); ?>" width="250" alt="">
+
+			<div class="col-md-4">
+				<div class="img-thumbnail">
+					<img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/Squidward_Tentacles.svg/1200px-Squidward_Tentacles.svg.png" alt="" class="img-fluid rounded-circle ml-5 mt-3" width="250">
+					<br><br>
+					<div class="caption">
+						<p class="text-center">"Belajar di <i>Bahasaku</i> mengajarkanku apa artinya kenyamanan dan kesempurnaannya bahasa Indonesia. - <strong>Squidward</strong>, <i>Teknik budidaya ubur-ubur</i>"</p>
+					</div>
+				</div>
 			</div>
-			<div class="col-md-6">
-				<h4 class="pt-5 pr-5"><strong>Menigkatkan Imajinasi dan Prestasi</strong></h4>
-				<p class="pr-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ducimus quam illum
-					similique cumque ratione cupiditate dolorem voluptate pariatur hic, rerum, et tempora earum iure
-					ullam, id ea numquam, soluta!</p>
+
+		</div><br><br><br>
+
+		<div class="container">
+			<h2 class="text-center"><strong>Kenapa harus memilih kami?</strong></h2>
+			<hr class="hr-fitur">
+			<div class="row">
+				<div class="col-md-6">
+					<h4 class="pt-5 pr-5"><strong>Berfokus pada Bahasa Indonesia</strong></h4>
+					<p class="pr-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ducimus quam illum
+						similique cumque ratione cupiditate dolorem voluptate pariatur hic, rerum, et tempora earum iure
+						ullam, id ea numquam, soluta!</p>
+				</div>
+				<div class="col-md-6">
+					<img src="<?= base_url('assets/img/target.png'); ?>" width="200" alt="">
+				</div>
 			</div>
-		</div>
-	</div> <!-- akhir kontainer -->
+			<div class="row">
+				<div class="col-md-6">
+					<img src="<?= base_url('assets/img/gratiss.png'); ?>" width=" 200" alt="">
+				</div>
+				<div class="col-md-6">
+					<h4 class="pt-5 pr-5"><strong>Bebas Biaya</strong></h4>
+					<p class="pr-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ducimus quam illum
+						similique cumque ratione cupiditate dolorem voluptate pariatur hic, rerum, et tempora earum iure
+						ullam, id ea numquam, soluta!</p>
+				</div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-md-6">
+					<h4 class="pt-5"><strong>Untuk Segala Jenjang</strong></h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ducimus quam illum
+						similique cumque ratione cupiditate dolorem voluptate pariatur hic, rerum, et tempora earum iure
+						ullam, id ea numquam, soluta!</p>
+				</div>
+				<div class="col-md-6">
+					<img src="<?= base_url('assets/img/luas.png'); ?>" width="200" alt="">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<img src="<?= base_url('assets/img/random.png'); ?>" width="250" alt="">
+				</div>
+				<div class="col-md-6">
+					<h4 class="pt-5 pr-5"><strong>Meningkatkan Imajinasi dan Prestasi</strong></h4>
+					<p class="pr-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ducimus quam illum
+						similique cumque ratione cupiditate dolorem voluptate pariatur hic, rerum, et tempora earum iure
+						ullam, id ea numquam, soluta!</p>
+				</div>
+			</div>
+		</div> <!-- akhir kontainer -->
 	</div>
 	</div> <!-- akhir kontainer -->
 	<br><br>
